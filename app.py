88e36aeab8b02d879f42d353f152c4aaeba93cf0
Uemit Seren <uemit.seren@gmail.com>
2016-02-22 15:51:07 +0100

    Initial checkin

--- app.py
+++ app.py
@@ -1,6 +1,53 @@
-import falcon
+import hug
 import genotype
+import os
+from thehonestgenepipeline import imputation as imp
+from thehonestgenepipeline import ancestry as anc
+from celery.result import AsyncResult
 
-api = application = falcon.API()
+STORAGE_PATH = os.environ['STORAGE_PATH'] 
+genotype.STORAGE_PATH = '%s/GENOTYPES/ORIGINAL' % STORAGE_PATH
 
-api.add_route('/genotype',genotype.GenotypeResource('data','EKegFrJF7wsjBL8Z9HX0JiGlLEuwkNTsV5D5qjhy5Pk='))
+WEIGHTS = 'weights.hdf5'
+PCS_FILE='1k_genomes_pcs.hdf5' 
+
+@hug.post('/genotype')
+def upload_genotype(body):
+    '''Upload a genotype'''
+    return genotype.get_genotype_infos(genotype.upload_genotype(body))
+
+
+@hug.get('/genotype')
+def get_genotype_infos(id):
+    '''Retrieve information about a specific genotype'''
+    return genotype.get_genotype_infos(id)
+    
+    
+@hug.post('/imputation')
+def run_imputation(id):
+    '''Start the imputation process'''
+    res = imp.imputation.delay(id)
+    return {'id':res.id,'state':res.state}
+
+@hug.get('/imputation')
+def get_imputation_state(task_id):
+    '''Get the current state of the imputation'''
+    res = imp.imputation.AsyncResult(task_id)
+    state = {'id':res.id,'state':res.state}
+    if state['state'] == 'SUCCESS':
+        state['data'] =  res.get()
+    return state
+
+@hug.post('/ancestry')
+def run_ancestry(id):
+    '''Start the ancestry'''
+    res = anc.analysis.delay(id,WEIGHTS,PCS_FILE)
+    return {'id':res.id,'state':res.state}
+
+@hug.get('/ancestry')
+def get_ancestry_state(task_id):
+    res = anc.analysis.AsyncResult(task_id)
+    state = {'id':res.id,'state':res.state}
+    if state['state'] == 'SUCCESS':
+        state['data'] = res.get()    
+    return state
--- genotype.py
+++ genotype.py
@@ -1,66 +1,27 @@
-import falcon
 import uuid
 import os
-import h5py
+from imputor.core import impute
 from cryptography.fernet import Fernet
+
+STORAGE_PATH='/DATA'
 
 def _generate_id():
     return str(uuid.uuid4())
 
+def upload_genotype(data):
+    genotype_id = _generate_id()
+    filename = genotype_id
 
-
-class GenotypeResource(object):
-
-    def __init__(self,storage_path,key):
-        self.storage_path = storage_path
-        self.fernat = Fernet(key)
-
-    def on_post(self,req,resp):
-        genotype_id = _generate_id()
-        filename = genotype_id
-     
-        genotype_path = os.path.join(self.storage_path, filename)
-        data = req.stream.read()
-        self.convert_to_hdf5(data,genotype_path)
-        #token = self.fernat.encrypt(data)
+    genotype_path = os.path.join(STORAGE_PATH, '%s.hdf5' % filename)
+    #data = req.stream.read()
+    impute.convert_genotype_to_hdf5(data,genotype_path)
+    #token = self.fernat.encrypt(data)
         #with open(genotype_path, 'wb') as genotype_file:
-         #   genotype_file.write(token)
-
-        resp.status = falcon.HTTP_201
-        resp.body = genotype_id
-
-    def on_get(self,req,resp):
-        resp.body = '{"message": "Hello world!"}'
-        resp.status = falcon.HTTP_200
-        
+    #   genotype_file.write(token)
+    return genotype_id
     
-    def convert_to_hdf5(self,data,genotype_id):
-        csv_content = data.decode("utf-8")
-        start_chr = None
-        f = h5py.File('%s.hdf5' % genotype_id)
-        pos_snps =[]
-        
-        for row in csv_content.splitlines():
-            if len(row) == 0 or row[0] == '#':
-                continue
-            cols = row.split("\t")
-            if len(cols) != 4:
-                continue        
-            if cols[1] != start_chr:
-                if start_chr is not None:
-                    sorted(pos_snps, key=lambda x: x[0])
-                    positions,snps = zip(*pos_snps)
-                    pos_dset = group.create_dataset('positions',(len(positions),),chunks=True,compression='lzf',dtype='i8',data=positions)
-                    snp_dset = group.create_dataset('snps',(len(positions),),chunks=True,compression='lzf',dtype='S2',data=snps)
-                    pos_snps =[]   
-                start_chr = cols[1]
-                group = f.create_group("Chr%s" % start_chr)
-            pos_snps.append((int(cols[2]),cols[3].encode('utf-8')))
-        sorted(pos_snps, key=lambda x: x[0])
-        positions,snps = zip(*pos_snps)   
-        pos_dset = group.create_dataset('positions',(len(positions),),chunks=True,compression='lzf',dtype='i8',data=positions)
-        snp_dset = group.create_dataset('snps',(len(positions),),chunks=True,compression='lzf',dtype='S2',data=snps)
-                
-            
-            
-        
+def get_genotype_infos(id):
+    return {'genotype':id}
+
+
+
