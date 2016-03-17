164398a7a03f7156e23f0ed758eb27e854e2647d
Uemit Seren @ Laptop <uemit.seren@gmail.com>
2016-03-17 08:02:19 +0100

    Added Cloud support

--- app.py
+++ app.py
@@ -4,21 +4,65 @@ import os
 from thehonestgenepipeline import imputation as imp
 from thehonestgenepipeline import ancestry as anc
 from celery.result import AsyncResult
+import cloud
+import falcon 
 
 STORAGE_PATH = os.environ['STORAGE_PATH'] 
 genotype.STORAGE_PATH = '%s/GENOTYPES/ORIGINAL' % STORAGE_PATH
 
-WEIGHTS = 'weights.hdf5'
-PCS_FILE='1k_genomes_pcs.hdf5' 
+WEIGHTS = 'european_weights.hdf5'
+PCS_FILE='hapmap_european_pcs.hdf5' 
+
+
+
+import cgi
+
+@hug.directive()
+def getfile(name='file', request = None,**kwargs):
+    '''Returns passed in parameter multiplied by itself'''
+    # TODO: Either validate that content type is multipart/form-data
+    # here, or in another hook before allowing execution to proceed.
+
+    # This must be done to avoid a bug in cgi.FieldStorage
+    env = request.env
+    env.setdefault('QUERY_STRING', '')
+
+    # TODO: Add error handling, when the request is not formatted
+    # correctly or does not contain the desired field...
+
+    # TODO: Consider overriding make_file, so that you can
+    # stream directly to the destination rather than
+    # buffering using TemporaryFile (see http://goo.gl/Yo8h3P)
+    form = cgi.FieldStorage(fp=request.stream, environ=env)
+
+    file_item = form[name]
+    if file_item.file:
+        body = file_item.file.read()
+        # It's an uploaded file
+        return body
+    else:
+        # TODO: Raise an error
+        pass
+
+
+GENOTYPE_PROVIDERS = {'23andme':cloud.CloudResource('d892eef346313a58fae9340140955cf0','ca3fe941649f96541961f797e5fb0beb','http://localhost:5000/receive_code/','genomes basic names','https://api.23andme.com/1/demo/')}
+
+@hug.directive()
+def cloud_provider(provider='', **kwargs):
+    '''checks if the provider is available'''
+    if provider not in GENOTYPE_PROVIDERS:
+        raise Exception('Cloud provider %s not found' % provider)
+    return GENOTYPE_PROVIDERS[provider]    
 
 @hug.post('/genotype')
-def upload_genotype(body):
+def upload_genotype(body:getfile):
     '''Upload a genotype'''
-    return genotype.get_genotype_infos(genotype.upload_genotype(body))
+    return genotype.get_genotype_infos(genotype.upload_genotype(body.decode("utf-8")))
 
 
 @hug.get('/genotype')
 def get_genotype_infos(id):
+    print('get genotype')
     '''Retrieve information about a specific genotype'''
     return genotype.get_genotype_infos(id)
     
@@ -51,3 +95,22 @@ def get_ancestry_state(task_id):
     if state['state'] == 'SUCCESS':
         state['data'] = res.get()    
     return state
+
+
+@hug.post('/cloud/{provider}/token')
+def get_token_for_provider(provider:cloud_provider ):
+    '''Returns access token for specified provider'''
+    return provider.get_token()
+    
+@hug.get('/cloud/{provider}/genotypes')
+def get_genotypes_for_provider(provider:cloud_provider,request):
+    '''Retrieves genotypes for specified provider'''
+    return provider.get_genotypes(request['ACCESS_TOKEN'])
+
+@hug.get('/cloud/{provider}/genome/{id}')
+def transfer_genome(provider:cloud_provider,request):
+    '''Uploads genotype data for specified provider'''
+    data = provider.get_genotype_data(request['ACCESS_TOKEN'],id)
+    return genotype.get_genotype_infos(data)
+
+
---
+++ cloud.py
@@ -0,0 +1,62 @@
+import os
+import h5py
+import requests
+import json
+import uuid
+import pandas as pd
+from cryptography.fernet import Fernet
+
+SNP_INDEX = pd.read_csv('snps_index.data',skiprows=3,delimiter='\t',index_col=0,encoding='utf-8')
+
+class CloudResource(object):
+
+    def __init__(self,client_secret,client_id,redirect_uri,scope,url):
+        self.url = url
+        self.client_secret = client_secret
+        self.client_id = client_id
+        self.redirect_uri = redirect_uri
+        self.scope = scope
+
+    def get_token(self,code):
+        code = req.headers['CODE']
+        payload = {'code':code,'grant_type':'authorization_code','client_id': self.client_id, 'client_secret': self.client_secret, 'redirect_uri':self.redirect_uri,'scope':self.scope}
+        res = requests.post('https://api.23andme.com/token/',data=payload)
+        if res.ok:
+            token_dict = res.json()
+            return token_dict
+        else:
+            raise Exception('Error retrieving token: %s' % res.text)
+            
+    def get_genotypes(self,access_token,refresh_token):
+         url = '%s/user/' % url
+         headers = {'Authorization':'Bearer %s' % token}
+         # filter for genotyped = true and retrieve more infos
+         res = requests.get(url,headers=headers)
+         if res.ok:
+             return res.json()
+         else:
+             raise Exception('Error retrieving profiles: %s' % res.text)
+    
+    def get_genotype_data(self,access_token,id):
+        id = req.params['id']
+        url = '%s/genomes/%s?unfiltered=true' % (URL,id)
+        headers = {'Authorization':'Bearer %s' % token}
+        res = requests.get(url,headers=headers)
+        if res.ok:
+            data = self._coordinate_with_index(res.json()['genome'])
+            return data
+        else:
+            raise Exception('Error retrieving genomes: %s ' % res.text)
+                
+    def _coordinate_with_index(self,genome):
+        alleles = [genome[ix:ix+2] for ix in range(0,len(genome),2)]
+        p = pd.Series(alleles,name='allele')
+        data = SNP_INDEX.join(p,how='inner')
+        fd = data.dropna()
+        fd = fd[(fd['chromosome'] != '0') & (fd['chromosome_position'] != 0)]
+        return fd
+
+
+        
+     
+
