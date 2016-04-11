653c7dafc1cbe09673314f7c6e37109037d2399d
Uemit Seren @ Laptop <uemit.seren@gmail.com>
2016-04-12 00:38:41 +0200

    Retrieve histogram of population risk for traits

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 from os import environ
 from configparser import ConfigParser 
 from configparser import RawConfigParser
+import csv
+import numpy as np
+
 
 STORAGE_PATH = environ.get('STORAGE_PATH','.')
 DATA_PATH = environ.get('DATA_PATH','%s/DATA' % STORAGE_PATH)
@@ -13,6 +16,13 @@ TRAIT_FILE = environ.get('TRAIT_FILE','./traits.ini')
 
 def _can_do_oauth(opts):
     return opts['client_secret'] != '' and opts['client_id'] and opts['redirect_url']!='' and opts['scope'] != '' and opts['oauth_url'] != ''
+    
+def _get_trait_histogram(trait):
+    histogram = [['Risk']]
+    with open('%s/%s.csv' % (DATA_PATH,trait)) as f:
+        for row in csv.reader(f):
+            histogram.append([float(row[0])])
+    return histogram
 
 parser = ConfigParser()
 parser.read(OAUTH_CONFIG)
@@ -37,6 +47,8 @@ for trait in traits:
     trait_dict = {'name':trait}
     for option in trait_parser.options(trait):
         trait_dict[option] = trait_parser.get(trait,option)
+    trait_dict['histogram'] = _get_trait_histogram(trait)
+    trait_dict['meanRisk'] = float(np.mean(np.asarray(trait_dict['histogram'][1:])))
     TRAITS.append(trait_dict)
    
  
